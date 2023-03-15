bc007d457541e2f8a850ad52ed2f565cb1941f67
ayaanqui <ayaanqui.com@gmail.com>
2023-03-15 00:01:52 -0500

    feat: create welcome screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { WelcomeScreen } from './screens/welcome/WelcomScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="welcome"
+          component={WelcomeScreen}
+          options={{
+            title: 'Welcome',
+            // headerShown: false,
+          }}
+        />
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/welcome/WelcomScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export function WelcomeScreen() {
+  return (
+    <View>
+      <Text>Welcome!</Text>
+    </View>
+  )
+}
