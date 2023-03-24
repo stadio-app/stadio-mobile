fea1ca1cd0095d5a72fbeaf569cebe0285ed0984
ayaanqui <ayaanqui.com@gmail.com>
2023-03-23 23:47:37 -0500

    feat: setup apollo provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'localhost:8080/graphql',
+  cache: new InMemoryCache(),
+});
 
 import { RootStackParamList } from './types/RootStackParamList';
 import * as Screens from './screens/screens';
@@ -9,23 +15,25 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Login'
-          component={Screens.LoginScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name='MainMenu'
-          component={Screens.MainMenu}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Screens.LoginScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="MainMenu"
+            component={Screens.MainMenu}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
