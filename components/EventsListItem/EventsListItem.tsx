d268f00ea025945dc4882dad3c91e74566455405
Mohammed Amarnah <m.amarnah@gmail.com>
2023-03-24 14:58:01 -0500

    Fix list item icons alignment

--- components/EventsListItem/EventsListItem.tsx
+++ components/EventsListItem/EventsListItem.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { ListItem, Image } from '@rneui/themed';
+import { ListItem } from '@rneui/themed';
 import {
   Ionicons,
   EvilIcons,
@@ -8,8 +8,13 @@ import {
 } from '@expo/vector-icons';
 import { EventsListItemType } from '../../types/EventsListItemType';
 import { Avatar } from '@rneui/base';
+import { ListItemChevron } from '@rneui/base/dist/ListItem/ListItem.Chevron';
 
 export default function EventsListItem({ id }: EventsListItemType) {
+  type PropsWithChildren<P> = P & { children?: ReactNode };
+  const IconView = ({ children }: PropsWithChildren<{}>) => {
+    return <View style={styles.iconView}>{children}</View>;
+  };
   return (
     <View key={id} style={styles.listItem}>
       {(parseInt(id) === 1 || parseInt(id) % 3 == 0) && (
@@ -19,13 +24,30 @@ export default function EventsListItem({ id }: EventsListItemType) {
         <Avatar size={100} source={require('../../assets/icon.png')} />
         <ListItem.Content>
           <ListItem.Title>Naperville Yard Indoor Sports Complex</ListItem.Title>
-          <ListItem.Subtitle>
-            <EvilIcons name="location" />
-            <Ionicons name="time-outline" />
-            <Ionicons name="people" />
-            <MaterialCommunityIcons name="soccer-field" />
+          <ListItem.Subtitle style={styles.contentGrid}>
+            <View>
+              <IconView>
+                <EvilIcons name="location" />
+                <Text>Naperville, IL</Text>
+              </IconView>
+              <IconView>
+                <Ionicons name="time-outline" />
+                <Text>9:30 PM</Text>
+              </IconView>
+            </View>
+            <View>
+              <IconView>
+                <Ionicons name="people" />
+                <Text>10/15</Text>
+              </IconView>
+              <IconView>
+                <MaterialCommunityIcons name="soccer-field" />
+                <Text>Indoor</Text>
+              </IconView>
+            </View>
           </ListItem.Subtitle>
         </ListItem.Content>
+        <ListItemChevron />
       </ListItem>
     </View>
   );
@@ -41,5 +63,17 @@ const styles = StyleSheet.create({
       paddingRight: 10,
       marginTop: 20,
     },
+  },
+  contentGrid: {
+    marginTop: 10,
+  },
+  iconView: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+    marginTop: 5,
+    marginLeft: 5,
   },
 });
