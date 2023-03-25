ca6adb6df941039651fcbf37fca65163d518db17
ayaanqui <ayaanqui.com@gmail.com>
2023-03-25 08:23:23 -0500

    feat: configure codegen

--- apollo/config.ts
+++ apollo/config.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: 'localhost:8080/graphql',
+  uri: 'http://localhost:8080/graphql',
   cache: new InMemoryCache(),
 });
--- tabs/favorites/Favorites.tsx
+++ tabs/favorites/Favorites.tsx
@@ -1,9 +1,38 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { View } from 'react-native';
+import { GetAllUsersQuery } from '../../generated/graphql';
+
+const GET_ALL_USERS = gql`
+  query GetAllUsers {
+    users {
+      id
+      email
+      phoneNumber
+      name
+      avatar
+      active
+      owner {
+        id
+        firstName
+        lastName
+        middleName
+        verified
+        locations {
+          id
+          name
+          address {
+            id
+            latitude
+            longitude
+          }
+        }
+      }
+    }
+  }
+`;
 
 export function Favorites() {
-  return (
-    <View>
-    </View>
-  )
+  const { loading, data, error } = useQuery<GetAllUsersQuery>(GET_ALL_USERS);
+  return <View></View>;
 }
