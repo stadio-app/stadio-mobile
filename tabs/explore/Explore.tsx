22c078b0ce88e4e0505af81343db28bf4ae1b54b
Ayaan Siddiqui <=>
2024-04-17 21:52:57 -0500

    feat: get explore page events

--- tabs/explore/Explore.tsx
+++ tabs/explore/Explore.tsx
@@ -11,9 +11,55 @@ import { SearchBar } from '@rneui/themed';
 import { persons } from './temp_data';
 import EventsListItem from '../../components/EventsListItem/EventsListItem';
 import { Ionicons } from '@expo/vector-icons';
+import { gql, useQuery } from '@apollo/client';
+import { EventsQuery, EventsQueryVariables } from '../../generated/graphql';
+
+const ALL_EVENTS = gql`
+  query Events($filters: AllEventsFilter!) {
+    allEvents(filter: $filters) {
+      id
+      type
+      startDate
+      endDate
+      location {
+        id
+        name
+        address {
+          id
+          latitude
+          longitude
+          fullAddress
+          mapsLink
+          countryCode
+          country
+        }
+      }
+      createdBy {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export function Explore() {
   const [search, setSearch] = useState('');
+  const { data, loading, error } = useQuery<EventsQuery, EventsQueryVariables>(
+    ALL_EVENTS,
+    {
+      variables: {
+        filters: {
+          radiusMeters: 6_000_000,
+          countryCode: 'US',
+          latitude: 41.8823144, // See https://docs.expo.dev/versions/latest/sdk/location/ to get user's geolocation
+          longitude: -87.6346181,
+          endDate: '2024-06-01T00:00:00.674Z',
+          startDate: '2024-01-01T00:00:00.674Z',
+        },
+      },
+    }
+  );
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
