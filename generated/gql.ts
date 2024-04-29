d223405a2911c2df552da2fa2958aa9925e16448
ayaanqui <ayaanqui.com@gmail.com>
2024-04-29 12:14:23 -0500

    feat: re run codegen

--- generated/gql.ts
+++ generated/gql.ts
@@ -13,20 +13,13 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  '\n  mutation CreateAccount($email: String!, $name: String!, $password: String!) {\n    createAccount(input:{\n      email: $email,\n      name: $name,\n      password: $password\n    }) {\n      id\n      name\n      email\n      phoneNumber\n      createdAt\n      updatedAt\n      authPlatform\n    }\n  }\n':
-    types.CreateAccountDocument,
-  '\n  mutation VerifyEmail($verificationCode: String!) {\n    verifyEmail(verificationCode: $verificationCode) {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n':
-    types.VerifyEmailDocument,
-  '\n  mutation ResendVerification($email: String!) {\n    resendEmailVerificationCode(email: $email)\n  }\n':
-    types.ResendVerificationDocument,
-  '\n  query Login($email: String!, $password: String!) {\n    login(email: $email, password: $password) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n    }\n  }\n':
-    types.LoginDocument,
-  '\n  query Me {\n    me {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n':
-    types.MeDocument,
-  '\n  query GoogleOauth($accessToken: String!) {\n    googleOAuth(accessToken: $accessToken) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n      isNewUser\n    }\n  }\n':
-    types.GoogleOauthDocument,
-  '\n  query Events($filters: AllEventsFilter!) {\n    allEvents(filter: $filters) {\n      id\n      type\n      startDate\n      endDate\n      location {\n        id\n        name\n        address {\n          id\n          latitude\n          longitude\n          fullAddress\n          mapsLink\n          countryCode\n          country\n        }\n      }\n      createdBy {\n        id\n        name\n      }\n    }\n  }\n':
-    types.EventsDocument,
+    "\n  mutation CreateAccount($email: String!, $name: String!, $password: String!) {\n    createAccount(input:{\n      email: $email,\n      name: $name,\n      password: $password\n    }) {\n      id\n      name\n      email\n      phoneNumber\n      createdAt\n      updatedAt\n      authPlatform\n    }\n  }\n": types.CreateAccountDocument,
+    "\n  mutation VerifyEmail($verificationCode: String!) {\n    verifyEmail(verificationCode: $verificationCode) {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n": types.VerifyEmailDocument,
+    "\n  mutation ResendVerification($email: String!) {\n    resendEmailVerificationCode(email: $email)\n  }\n": types.ResendVerificationDocument,
+    "\n  query Login($email: String!, $password: String!) {\n    login(email: $email, password: $password) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n    }\n  }\n": types.LoginDocument,
+    "\n  query Me {\n    me {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n": types.MeDocument,
+    "\n  query GoogleOauth($accessToken: String!) {\n    googleOAuth(accessToken: $accessToken) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n      isNewUser\n    }\n  }\n": types.GoogleOauthDocument,
+    "\n  query Events($filters: AllEventsFilter!) {\n    allEvents(filter: $filters) {\n      id\n      type\n      startDate\n      endDate\n      location {\n        id\n        name\n        address {\n          id\n          latitude\n          longitude\n          fullAddress\n          mapsLink\n          countryCode\n          country\n        }\n      }\n      createdBy {\n        id\n        name\n      }\n    }\n  }\n": types.EventsDocument,
 };
 
 /**
@@ -46,49 +39,34 @@ export function gql(source: string): unknown;
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  mutation CreateAccount($email: String!, $name: String!, $password: String!) {\n    createAccount(input:{\n      email: $email,\n      name: $name,\n      password: $password\n    }) {\n      id\n      name\n      email\n      phoneNumber\n      createdAt\n      updatedAt\n      authPlatform\n    }\n  }\n'
-): (typeof documents)['\n  mutation CreateAccount($email: String!, $name: String!, $password: String!) {\n    createAccount(input:{\n      email: $email,\n      name: $name,\n      password: $password\n    }) {\n      id\n      name\n      email\n      phoneNumber\n      createdAt\n      updatedAt\n      authPlatform\n    }\n  }\n'];
+export function gql(source: "\n  mutation CreateAccount($email: String!, $name: String!, $password: String!) {\n    createAccount(input:{\n      email: $email,\n      name: $name,\n      password: $password\n    }) {\n      id\n      name\n      email\n      phoneNumber\n      createdAt\n      updatedAt\n      authPlatform\n    }\n  }\n"): (typeof documents)["\n  mutation CreateAccount($email: String!, $name: String!, $password: String!) {\n    createAccount(input:{\n      email: $email,\n      name: $name,\n      password: $password\n    }) {\n      id\n      name\n      email\n      phoneNumber\n      createdAt\n      updatedAt\n      authPlatform\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  mutation VerifyEmail($verificationCode: String!) {\n    verifyEmail(verificationCode: $verificationCode) {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n'
-): (typeof documents)['\n  mutation VerifyEmail($verificationCode: String!) {\n    verifyEmail(verificationCode: $verificationCode) {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n'];
+export function gql(source: "\n  mutation VerifyEmail($verificationCode: String!) {\n    verifyEmail(verificationCode: $verificationCode) {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n"): (typeof documents)["\n  mutation VerifyEmail($verificationCode: String!) {\n    verifyEmail(verificationCode: $verificationCode) {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  mutation ResendVerification($email: String!) {\n    resendEmailVerificationCode(email: $email)\n  }\n'
-): (typeof documents)['\n  mutation ResendVerification($email: String!) {\n    resendEmailVerificationCode(email: $email)\n  }\n'];
+export function gql(source: "\n  mutation ResendVerification($email: String!) {\n    resendEmailVerificationCode(email: $email)\n  }\n"): (typeof documents)["\n  mutation ResendVerification($email: String!) {\n    resendEmailVerificationCode(email: $email)\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query Login($email: String!, $password: String!) {\n    login(email: $email, password: $password) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n    }\n  }\n'
-): (typeof documents)['\n  query Login($email: String!, $password: String!) {\n    login(email: $email, password: $password) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n    }\n  }\n'];
+export function gql(source: "\n  query Login($email: String!, $password: String!) {\n    login(email: $email, password: $password) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n    }\n  }\n"): (typeof documents)["\n  query Login($email: String!, $password: String!) {\n    login(email: $email, password: $password) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query Me {\n    me {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n'
-): (typeof documents)['\n  query Me {\n    me {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n'];
+export function gql(source: "\n  query Me {\n    me {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n"): (typeof documents)["\n  query Me {\n    me {\n      id\n      name\n      email\n      avatar\n      createdAt\n      updatedAt\n      active\n      authPlatform\n      authStateId\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query GoogleOauth($accessToken: String!) {\n    googleOAuth(accessToken: $accessToken) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n      isNewUser\n    }\n  }\n'
-): (typeof documents)['\n  query GoogleOauth($accessToken: String!) {\n    googleOAuth(accessToken: $accessToken) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n      isNewUser\n    }\n  }\n'];
+export function gql(source: "\n  query GoogleOauth($accessToken: String!) {\n    googleOAuth(accessToken: $accessToken) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n      isNewUser\n    }\n  }\n"): (typeof documents)["\n  query GoogleOauth($accessToken: String!) {\n    googleOAuth(accessToken: $accessToken) {\n      token\n      user {\n        id\n        name\n        email\n        avatar\n        createdAt\n        updatedAt\n        active\n        authPlatform\n        authStateId\n      }\n      isNewUser\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query Events($filters: AllEventsFilter!) {\n    allEvents(filter: $filters) {\n      id\n      type\n      startDate\n      endDate\n      location {\n        id\n        name\n        address {\n          id\n          latitude\n          longitude\n          fullAddress\n          mapsLink\n          countryCode\n          country\n        }\n      }\n      createdBy {\n        id\n        name\n      }\n    }\n  }\n'
-): (typeof documents)['\n  query Events($filters: AllEventsFilter!) {\n    allEvents(filter: $filters) {\n      id\n      type\n      startDate\n      endDate\n      location {\n        id\n        name\n        address {\n          id\n          latitude\n          longitude\n          fullAddress\n          mapsLink\n          countryCode\n          country\n        }\n      }\n      createdBy {\n        id\n        name\n      }\n    }\n  }\n'];
+export function gql(source: "\n  query Events($filters: AllEventsFilter!) {\n    allEvents(filter: $filters) {\n      id\n      type\n      startDate\n      endDate\n      location {\n        id\n        name\n        address {\n          id\n          latitude\n          longitude\n          fullAddress\n          mapsLink\n          countryCode\n          country\n        }\n      }\n      createdBy {\n        id\n        name\n      }\n    }\n  }\n"): (typeof documents)["\n  query Events($filters: AllEventsFilter!) {\n    allEvents(filter: $filters) {\n      id\n      type\n      startDate\n      endDate\n      location {\n        id\n        name\n        address {\n          id\n          latitude\n          longitude\n          fullAddress\n          mapsLink\n          countryCode\n          country\n        }\n      }\n      createdBy {\n        id\n        name\n      }\n    }\n  }\n"];
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
